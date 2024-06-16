e58f7f0ef99fc42aee357fe65c25580900ca760c
hubertkrajnik <hubert.krajnik@gmail.com>
2024-06-16 14:06:19 +0200

    Button to Link "Odkrywaj"

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import { Link } from 'react-scroll'
 
 const Hero = () => {
 return (
@@ -7,7 +8,9 @@ return (
         <div className='hero-text'>
             <h1>Odkryj studenckie kulinaria w Toruniu!</h1>
             <p>Dzięki naszej stronie chcemy udostępnić wszystkim propzycje na zjedzenie czegoś smacznego w mieście Toruń. Nasz wyspecjalizowany zespół kierował się najlepszym stosunkiem cena-jakość.</p>
-            <button className='btn'>Odkrywaj</button>
+            <Link to='restaurant-list' smooth={true} offset={-300} duration={500} className='btn'>
+                    Odkrywaj
+            </Link>
         </div>
     </div>
 )
