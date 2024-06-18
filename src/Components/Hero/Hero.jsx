0a5c6604129d16a8e89f7fe9ed18d75897594192
hubertkrajnik <hubert.krajnik@gmail.com>
2024-06-18 20:33:45 +0200

    Content update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ return (
     <div className='hero container'>
         <div className='hero-text'>
             <h1>Odkryj studenckie kulinaria w Toruniu!</h1>
-            <p>Dzięki naszej stronie chcemy udostępnić wszystkim propzycje na zjedzenie czegoś smacznego w mieście Toruń. Nasz wyspecjalizowany zespół kierował się najlepszym stosunkiem cena-jakość.</p>
+            <p>Dzięki naszej stronie chcemy udostępnić wszystkim propozycje na zjedzenie czegoś smacznego w mieście Toruń. Nasz wyspecjalizowany zespół kierował się najlepszym stosunkiem cena-jakość.</p>
             <Link to='restaurant-list' smooth={true} offset={-300} duration={500} className='btn'>
                     Odkrywaj
             </Link>
